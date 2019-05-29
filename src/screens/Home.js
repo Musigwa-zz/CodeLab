69f3ce2017f822a9da4f0214afd9bc524a2a9f32
MUSIGWA Pacifique <pacifique.musigwa@gmail.com>
2019-05-29 19:10:14 +0200

    chore(ch): configure eslint and integrate hound ci

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,156 +1,147 @@
-import React, { Component } from "react";
-import {
-  StyleSheet,
-  Dimensions,
-  Text,
-  View,
-  FlatList,
-  Image,
-  ActivityIndicator,
-  SafeAreaView
-} from "react-native";
-import { colors, metrics } from "../helpers/constants";
+import React, { Component } from 'react';
+import { StyleSheet, Dimensions, Text, View, FlatList, Image, SafeAreaView } from 'react-native';
+import { colors, metrics } from '../helpers/constants';
 
 const developers = [
   {
-    username: "Musigwa",
-    fullName: "Musigwa Pacifique",
-    email: "pacifique.musigwa@gmail.com",
-    avatar: "https://avatars1.githubusercontent.com/u/6963510?v=4"
+    username: 'Musigwa',
+    fullName: 'Musigwa Pacifique',
+    email: 'pacifique.musigwa@gmail.com',
+    avatar: 'https://avatars1.githubusercontent.com/u/6963510?v=4',
   },
   {
-    username: "Musigwa",
-    fullName: "Musigwa Pacifique",
-    email: "pacifique.musigwa@gmail.com",
-    avatar: "https://avatars1.githubusercontent.com/u/6963510?v=4"
+    username: 'Musigwa',
+    fullName: 'Musigwa Pacifique',
+    email: 'pacifique.musigwa@gmail.com',
+    avatar: 'https://avatars1.githubusercontent.com/u/6963510?v=4',
   },
   {
-    username: "Musigwa",
-    fullName: "Musigwa Pacifique",
-    email: "pacifique.musigwa@gmail.com",
-    avatar: "https://avatars1.githubusercontent.com/u/6963510?v=4"
+    username: 'Musigwa',
+    fullName: 'Musigwa Pacifique',
+    email: 'pacifique.musigwa@gmail.com',
+    avatar: 'https://avatars1.githubusercontent.com/u/6963510?v=4',
   },
   {
-    username: "Musigwa",
-    fullName: "Musigwa Pacifique",
-    email: "pacifique.musigwa@gmail.com",
-    avatar: "https://avatars1.githubusercontent.com/u/6963510?v=4"
+    username: 'Musigwa',
+    fullName: 'Musigwa Pacifique',
+    email: 'pacifique.musigwa@gmail.com',
+    avatar: 'https://avatars1.githubusercontent.com/u/6963510?v=4',
   },
   {
-    username: "Musigwa",
-    fullName: "Musigwa Pacifique",
-    email: "pacifique.musigwa@gmail.com",
-    avatar: "https://avatars1.githubusercontent.com/u/6963510?v=4"
+    username: 'Musigwa',
+    fullName: 'Musigwa Pacifique',
+    email: 'pacifique.musigwa@gmail.com',
+    avatar: 'https://avatars1.githubusercontent.com/u/6963510?v=4',
   },
   {
-    username: "Musigwa",
-    fullName: "Musigwa Pacifique",
-    email: "pacifique.musigwa@gmail.com",
-    avatar: "https://avatars1.githubusercontent.com/u/6963510?v=4"
+    username: 'Musigwa',
+    fullName: 'Musigwa Pacifique',
+    email: 'pacifique.musigwa@gmail.com',
+    avatar: 'https://avatars1.githubusercontent.com/u/6963510?v=4',
   },
   {
-    username: "Musigwa",
-    fullName: "Musigwa Pacifique",
-    email: "pacifique.musigwa@gmail.com",
-    avatar: "https://avatars1.githubusercontent.com/u/6963510?v=4"
+    username: 'Musigwa',
+    fullName: 'Musigwa Pacifique',
+    email: 'pacifique.musigwa@gmail.com',
+    avatar: 'https://avatars1.githubusercontent.com/u/6963510?v=4',
   },
   {
-    username: "Musigwa",
-    fullName: "Musigwa Pacifique",
-    email: "pacifique.musigwa@gmail.com",
-    avatar: "https://avatars1.githubusercontent.com/u/6963510?v=4"
+    username: 'Musigwa',
+    fullName: 'Musigwa Pacifique',
+    email: 'pacifique.musigwa@gmail.com',
+    avatar: 'https://avatars1.githubusercontent.com/u/6963510?v=4',
   },
   {
-    username: "Musigwa",
-    fullName: "Musigwa Pacifique",
-    email: "pacifique.musigwa@gmail.com",
-    avatar: "https://avatars1.githubusercontent.com/u/6963510?v=4"
+    username: 'Musigwa',
+    fullName: 'Musigwa Pacifique',
+    email: 'pacifique.musigwa@gmail.com',
+    avatar: 'https://avatars1.githubusercontent.com/u/6963510?v=4',
   },
   {
-    username: "Musigwa",
-    fullName: "Musigwa Pacifique",
-    email: "pacifique.musigwa@gmail.com",
-    avatar: "https://avatars1.githubusercontent.com/u/6963510?v=4"
+    username: 'Musigwa',
+    fullName: 'Musigwa Pacifique',
+    email: 'pacifique.musigwa@gmail.com',
+    avatar: 'https://avatars1.githubusercontent.com/u/6963510?v=4',
   },
   {
-    username: "Musigwa",
-    fullName: "Musigwa Pacifique",
-    email: "pacifique.musigwa@gmail.com",
-    avatar: "https://avatars1.githubusercontent.com/u/6963510?v=4"
+    username: 'Musigwa',
+    fullName: 'Musigwa Pacifique',
+    email: 'pacifique.musigwa@gmail.com',
+    avatar: 'https://avatars1.githubusercontent.com/u/6963510?v=4',
   },
   {
-    username: "Musigwa",
-    fullName: "Musigwa Pacifique",
-    email: "pacifique.musigwa@gmail.com",
-    avatar: "https://avatars1.githubusercontent.com/u/6963510?v=4"
+    username: 'Musigwa',
+    fullName: 'Musigwa Pacifique',
+    email: 'pacifique.musigwa@gmail.com',
+    avatar: 'https://avatars1.githubusercontent.com/u/6963510?v=4',
   },
   {
-    username: "Musigwa",
-    fullName: "Musigwa Pacifique",
-    email: "pacifique.musigwa@gmail.com",
-    avatar: "https://avatars1.githubusercontent.com/u/6963510?v=4"
+    username: 'Musigwa',
+    fullName: 'Musigwa Pacifique',
+    email: 'pacifique.musigwa@gmail.com',
+    avatar: 'https://avatars1.githubusercontent.com/u/6963510?v=4',
   },
   {
-    username: "Musigwa",
-    fullName: "Musigwa Pacifique",
-    email: "pacifique.musigwa@gmail.com",
-    avatar: "https://avatars1.githubusercontent.com/u/6963510?v=4"
+    username: 'Musigwa',
+    fullName: 'Musigwa Pacifique',
+    email: 'pacifique.musigwa@gmail.com',
+    avatar: 'https://avatars1.githubusercontent.com/u/6963510?v=4',
   },
   {
-    username: "Musigwa",
-    fullName: "Musigwa Pacifique",
-    email: "pacifique.musigwa@gmail.com",
-    avatar: "https://avatars1.githubusercontent.com/u/6963510?v=4"
+    username: 'Musigwa',
+    fullName: 'Musigwa Pacifique',
+    email: 'pacifique.musigwa@gmail.com',
+    avatar: 'https://avatars1.githubusercontent.com/u/6963510?v=4',
   },
   {
-    username: "Musigwa",
-    fullName: "Musigwa Pacifique",
-    email: "pacifique.musigwa@gmail.com",
-    avatar: "https://avatars1.githubusercontent.com/u/6963510?v=4"
-  }
+    username: 'Musigwa',
+    fullName: 'Musigwa Pacifique',
+    email: 'pacifique.musigwa@gmail.com',
+    avatar: 'https://avatars1.githubusercontent.com/u/6963510?v=4',
+  },
 ];
 
-const { height, width } = Dimensions.get("window");
+const { width } = Dimensions.get('window');
 const styles = StyleSheet.create({
   container: { flex: 1 },
   listContainer: {
     flex: 1,
-    marginTop: 20
+    marginTop: 20,
   },
   text: {
-    textAlign: "left",
-    paddingVertical: 2
+    textAlign: 'left',
+    paddingVertical: 2,
   },
   listItem: {
     paddingVertical: 5,
     paddingHorizontal: 10,
-    justifyContent: "space-between",
-    alignItems: "center",
+    justifyContent: 'space-between',
+    alignItems: 'center',
     borderBottomColor: metrics({}).border.borderColor,
     borderBottomWidth: metrics({}).border.borderWith,
-    flexDirection: "row"
+    flexDirection: 'row',
   },
   avatar: { width: 50, height: 50, borderRadius: 25 },
   content: {},
   title: {
     fontSize: 18,
-    color: colors({ opacity: 0.9 }).secondaryText
+    color: colors({ opacity: 0.9 }).secondaryText,
   },
   subTitle: {
     fontSize: 16,
-    color: colors({ opacity: 0.5 }).secondaryText
+    color: colors({ opacity: 0.5 }).secondaryText,
   },
   separator: {
     width,
     height: 1,
-    backgroundColor: colors({ opacity: 0.5 }).secondaryText
-  }
+    backgroundColor: colors({ opacity: 0.5 }).secondaryText,
+  },
 });
 
 class Home extends Component {
   static navigationOptions = { header: null };
 
-  state = { loading: false };
+  state = {};
 
   renderItem = ({ item }) => (
     <View style={styles.listItem}>
