af0d52deb39fab3d291267b9a8876a7aca3b464f
MUSIGWA Pacifique <pacifique.musigwa@gmail.com>
2019-05-30 11:36:20 +0200

    chore(fix): separate data with home component

---
+++ src/styles/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { StyleSheet, Dimensions } from "react-native";
+import { colors, metrics } from "../helpers/constants";
+const { width } = Dimensions.get("window");
+
+export default StyleSheet.create({
+  container: { flex: 1 },
+  listContainer: {
+    flex: 1,
+    marginTop: 20
+  },
+  text: {
+    textAlign: "left",
+    paddingVertical: 2
+  },
+  listItem: {
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderBottomColor: metrics({}).border.borderColor,
+    borderBottomWidth: metrics({}).border.borderWith,
+    flexDirection: "row"
+  },
+  avatar: { width: 50, height: 50, borderRadius: 25 },
+  content: {},
+  title: {
+    fontSize: 18,
+    color: colors({ opacity: 0.9 }).secondaryText
+  },
+  subTitle: {
+    fontSize: 16,
+    color: colors({ opacity: 0.5 }).secondaryText
+  },
+  separator: {
+    width,
+    height: 1,
+    backgroundColor: colors({ opacity: 0.5 }).secondaryText
+  }
+});
